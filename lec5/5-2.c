3094cf659f9a6ae7b9324917f7fd1152479ce90b
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-05-18 07:26:52 +0900

    init commit

---
+++ lec5/5-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define N 3
+
+int main(void) {
+    int i, j, k;
+    double mu, tmp;
+
+    double a[4][4] = {{4.0, 2.0, 4.0, 2.0}, {6.0, -6.0, 6, 12}, {4, 3, 3, -3}, {1, 2, -1, 1}};
+    double b[4] = {6, 18, -0.5, 5.0};
+
+
+}
